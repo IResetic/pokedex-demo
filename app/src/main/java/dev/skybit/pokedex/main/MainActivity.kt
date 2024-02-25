ece82b12cc56e98420048f8895ee77c7df048daf
IResetic <iresetic.skybit@gmail.com>
2024-02-25 16:45:15 +0100

    Add splash screen

--- app/src/main/java/dev/skybit/pokedex/main/MainActivity.kt
+++ app/src/main/java/dev/skybit/pokedex/main/MainActivity.kt
@@ -3,13 +3,11 @@ package dev.skybit.pokedex.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dagger.hilt.android.AndroidEntryPoint
 import dev.skybit.pokedex.main.core.presentation.style.theme.PokedexTheme
 
@@ -17,29 +15,17 @@ import dev.skybit.pokedex.main.core.presentation.style.theme.PokedexTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             PokedexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                Box(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(text = "THis is pokedex")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PokedexTheme {
-        Greeting("Android")
     }
 }
